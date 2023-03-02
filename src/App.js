cc4c8e214cd15de21a57e55e06aecad0f1b92ff6
Nolan Knill <nolanknill@gmail.com>
2023-03-02 15:33:53 -0500

    filters out the selected plant from the plant list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import plants from "./data/plants.json";
 import PlantList from "./components/PlantList/PlantList";
+import { useState } from "react";
 
 function App() {
+  const [selectedPlant, setSelectedPlant] = useState(plants[0]);
+
   const changeSelectedPlant = (id) => {
     // TODO: Remove the selected plant with this id from the list
     // TODO:  set the new selected plant to the plant with matching id
@@ -9,10 +12,15 @@ function App() {
     alert(id);
   }
 
+  // Create a new array without the selected plant
+  const filteredPlants = plants.filter((plant) => {
+    return plant.id !== selectedPlant.id;
+  })
+
   return (
     <PlantList 
       changeSelectedPlant={changeSelectedPlant}
-      plants={plants}
+      plants={filteredPlants}
     />
   );
 }
